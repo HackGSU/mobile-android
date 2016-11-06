8246236d0ed9662c50d29a68a357e8ea95aabeab
Joshua King <joshua@antechdevelopment.com>
2016-11-06 13:49:15 -0500

    Updated the update SDK bash script

--- updateSDKRepos.sh
+++ updateSDKRepos.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+
+echo "Starting update SDK bash script";
+
 echo "y" | $ANDROID_HOME/tools/android update sdk -u -a -t `$ANDROID_HOME/tools/android list sdk --all | grep -m 1 Support | sed 's/.\([0-9]*\).*/\1/'`;
+
+echo "Finished update SDK bash script";
