cd421bc93786ca2631c38939877a27fb2cab8d04
pranathi <pvenigandla@gmail.com>
2017-03-29 16:19:27 -0400

    Zoom works now.

---
+++ app/src/main/java/com/hackgsu/fall2016/android/activities/PhotoViewActivity.java
@@ -0,0 +1,152 @@
+package com.hackgsu.fall2016.android.activities;
+
+import android.annotation.SuppressLint;
+import android.support.annotation.DrawableRes;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.github.chrisbanes.photoview.PhotoView;
+import com.hackgsu.fall2016.android.R;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class PhotoViewActivity extends AppCompatActivity {
+    private static final boolean AUTO_HIDE = true;
+
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    public static final String IMAGE_TO_VIEW = "image_to_view";
+    private final Handler mHideHandler = new Handler();
+    private PhotoView photoView;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            photoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_photo_view);
+
+        mVisible = true;
+        photoView = (PhotoView) findViewById(R.id.photo_view);
+
+        @DrawableRes int imageToViewResource = getIntent().getIntExtra(IMAGE_TO_VIEW, 0);
+
+        if (imageToViewResource == 0) {
+            finish();
+        }
+
+        photoView.setImageResource(imageToViewResource);
+        photoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggle();
+            }
+        });
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        photoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+}
--- app/src/main/java/com/hackgsu/fall2016/android/fragments/FacilityMapFragment.java
+++ app/src/main/java/com/hackgsu/fall2016/android/fragments/FacilityMapFragment.java
@@ -1,12 +1,17 @@
 package com.hackgsu.fall2016.android.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.hackgsu.fall2016.android.R;
+import com.hackgsu.fall2016.android.activities.PhotoViewActivity;
 import com.ncapdevi.fragnav.FragNavController;
+
+import static android.R.attr.button;
+import static android.R.attr.onClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,6 +42,32 @@ public class FacilityMapFragment extends BaseFragment {
 	@Override
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_facility_map, container, false);
+		View view = inflater.inflate(R.layout.fragment_facility_map, container, false);
+
+		View concourseCardView = view.findViewById(R.id.concourse_card_view);
+		View floor1CardView = view.findViewById(R.id.floor1_card_view);
+		View zoomButtonConcourse = view.findViewById(R.id.button_zoom_concourse);
+		View zoomButtonFloor1 = view.findViewById(R.id.button_zoom_floor1);
+		View.OnClickListener onClickListenerForConcourse = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getContext(), PhotoViewActivity.class);
+				intent.putExtra(PhotoViewActivity.IMAGE_TO_VIEW, R.drawable.ic_concourse);
+				startActivity(intent);
+			}
+		};
+		concourseCardView.setOnClickListener(onClickListenerForConcourse);
+		zoomButtonConcourse.setOnClickListener(onClickListenerForConcourse);
+		View.OnClickListener onClickListenerForFloor1 = new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(getContext(), PhotoViewActivity.class);
+				intent.putExtra(PhotoViewActivity.IMAGE_TO_VIEW, R.drawable.ic_floor_1);
+				startActivity(intent);
+			}
+		};
+		floor1CardView.setOnClickListener(onClickListenerForFloor1);
+		zoomButtonFloor1.setOnClickListener(onClickListenerForFloor1);
+		return view;
 	}
 }
